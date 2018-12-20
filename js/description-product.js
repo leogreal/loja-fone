72680a8185b5db0bdc3701d1a6cee087cfb8a154
Leonardo <leogreal@gmail.com>
2018-12-20 00:51:42 +0000

    classList.toggle, magia boa demais

--- js/description-product.js
+++ js/description-product.js
@@ -3,5 +3,5 @@ const $heart = window.document.querySelector(".-heart");
 $heart.addEventListener("click", handleClick);
 
 function handleClick() {
-    $heart.classList.add("-active");
+    $heart.classList.toggle("-active");
 }
