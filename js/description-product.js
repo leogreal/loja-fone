c26b96fda9988fb5f9dfe8fd20e6401b6a688046
Leonardo <leogreal@gmail.com>
2018-12-20 00:43:53 +0000

    Mudando o comportamento visual

--- js/description-product.js
+++ js/description-product.js
@@ -3,5 +3,5 @@ const $heart = window.document.querySelector(".-heart");
 $heart.addEventListener("click", handleClick);
 
 function handleClick() {
-    console.log('oi');
+    $heart.classList.add("-active");
 }
