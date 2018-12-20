19b83fd855a839f556a79a97c91394c2d6174d5b
Leonardo <leogreal@gmail.com>
2018-12-20 00:20:39 +0000

    Refatora geral

--- js/button-store.js
+++
@@ -1,8 +0,0 @@
-const $btnSecond = window.document.querySelector(".-second");
-const $actionLast = window.document.querySelector("a.-last");
- 
-$btnSecond.addEventListener("click", btnSecondHandleClick);
- 
-function btnSecondHandleClick() {
-    $actionLast.innerHTML = "Carrinho Cheio";
-}
