2b9bfdb0660207a42da71df662cf38b9566d9046
Leonardo <leogreal@gmail.com>
2018-12-20 00:02:01 +0000

    [013] - Desafio avançado

--- js/button-store.js
+++ js/button-store.js
@@ -1,7 +1,8 @@
 const $btnSecond = window.document.querySelector(".-second");
+const $actionLast = window.document.querySelector("a.-last");
  
 $btnSecond.addEventListener("click", btnSecondHandleClick);
  
 function btnSecondHandleClick() {
-    alert("Funcionou!");
+    $actionLast.innerHTML = "Carrinho Cheio";
 }
