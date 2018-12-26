a3ff3cebf98b0ba7e31aa3820c721f3a5dcbac67
Leonardo <leogreal@gmail.com>
2018-12-26 14:40:08 +0000

    [014] - Desafio Intermediário

--- js/description-product.js
+++ js/description-product.js
@@ -5,3 +5,17 @@ $heart.addEventListener("click", heartHandleClick);
 function heartHandleClick() {
     $heart.classList.toggle("-active");
 }
+
+
+/* Star Rank */
+const $stars = window.document.querySelector(".-star").childNodes;
+
+for (let i = 0; i < $stars.length; i++) {
+    $stars[i].addEventListener("click", function() {
+        if(this.src.includes("star.png")){
+            this.src = "img/star-active.png";
+        } else {
+            this.src = "img/star.png";
+        }
+    });        
+}
