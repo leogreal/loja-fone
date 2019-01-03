6efeb3780eb50a21c9b4c9d3891fa7ad6c118e6a
Leonardo <leogreal@gmail.com>
2019-01-03 17:33:06 +0000

    [017] - Desafio Avançado

--- js/header-store.js
+++ js/header-store.js
@@ -3,4 +3,10 @@ const $searchIcon = document.querySelector(".search > .icon");
 
 $searchIcon.addEventListener("click", () => {
     $search.classList.toggle("-active");
+
+    if ($searchIcon.innerHTML == "🔍") {
+        $searchIcon.innerHTML = "❌"
+    } else {
+        $searchIcon.innerHTML = "🔍"
+    }
 });
